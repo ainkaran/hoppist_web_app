0ac1e0d4a77116851a189387f8f096a54a2c6e68
Alex Taylor <alexmctaylorpants@gmail.com>
2015-12-12 13:23:03 -0800

    Add more beers to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 Beer.destroy_all # also destroys reviews
 Brewery.destroy_all
 
-10.times do
+100.times do
   beer = FactoryGirl.create(:beer)
   10.times do
     FactoryGirl.create(:review, beer: beer)
