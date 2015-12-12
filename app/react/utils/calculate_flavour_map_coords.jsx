9cb30cfa08efb8ab24bba708abee96ef51e81ea0
Alex Taylor <alexmctaylorpants@gmail.com>
2015-12-11 17:57:24 -0800

    Adds util for calculating coordinates from db

---
+++ app/react/utils/calculate_flavour_map_coords.jsx
@@ -0,0 +1,13 @@
+'use strict'
+module.exports = function(flavour_x, colour_y, map_width, map_height) {
+  const FLAVOUR_SCALE = 12;
+  const COLOUR_SCALE  = 12;
+  var mapX = (flavour_x/FLAVOUR_SCALE)*map_width;
+  // we have to invert the value of Y here because the colour is stored in
+  // the db as light=12, dark=0, but it's the opposite on our map.
+  var mapY = ((COLOUR_SCALE - colour_y)/12)*map_height;
+  return {
+    x: mapX,
+    y: mapY
+  };
+};
