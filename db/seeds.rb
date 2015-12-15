015c340e74e5b9f0fc4395afbc92cc84c1c9d40f
Alex Taylor <alexmctaylorpants@gmail.com>
2015-12-14 19:24:55 -0800

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,30 @@
-# Beer.destroy_all # also destroys reviews
-# Brewery.destroy_all
-#
-# 100.times do
-#   beer = FactoryGirl.create(:beer)
-#   10.times do
-#     FactoryGirl.create(:review, beer: beer)
-#   end
-# end
+Beer.destroy_all # also destroys reviews
+Brewery.destroy_all
+Category.destroy_all
 
-categories = ["stout",
-               "porter",
-               "ale",
+categories = ["Stout",
+               "Porter",
+               "Ale",
                "IPA",
-               "lager",
-               "pilsner",
-               "bock",
-               "sour",
-               "other"]
+               "Lager",
+               "Pilsner",
+               "Bock",
+               "Sour",
+               "Saison",
+              "Hefeweizen",
+            "Other"]
 
 categories.each do |cat|
   Category.create!(name: cat)
+end
+
+25.times do
+  brewery = FactoryGirl.create(:brewery)
+
+  (rand(15) + 1).times do
+    beer = FactoryGirl.create(:beer, brewery: brewery, category: Category.all.sample)
+    (rand(20) + 1).times do
+      FactoryGirl.create(:review, beer: beer)
+    end
+  end
 end
