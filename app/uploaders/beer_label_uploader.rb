721c1224e412777a6818d0fb4fba5c3f569815df
Alex Taylor <alexmctaylorpants@gmail.com>
2015-12-14 20:13:39 -0800

    Adds fallback beer image

--- app/uploaders/beer_label_uploader.rb
+++ app/uploaders/beer_label_uploader.rb
@@ -57,17 +57,12 @@ class BeerLabelUploader < CarrierWave::Uploader::Base
   end
 
   def filename
-    "#{model.beer_label_filename}.jpg"
+    "#{model.beer_label_filename}.jpg" if original_filename
   end
 
-  # TODO: make a fallback image
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url
+    "/images/fallback/" + [version_name, "beer_default.jpg"].compact.join('_')
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
