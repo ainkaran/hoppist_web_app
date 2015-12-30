9423d947d8a6d3ae50dc8364ad71bff7395bd657
Alex Taylor <alexmctaylorpants@gmail.com>
2015-12-30 11:48:07 -0800

    Adds Goole OAuth2 strategy

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :twitter, ENV['TWITTER_API_CONSUMER_KEY'], ENV['TWITTER_API_CONSUMER_SECRET']
+  provider :twitter,       ENV['TWITTER_API_CONSUMER_KEY'], ENV['TWITTER_API_CONSUMER_SECRET']
+  provider :google_oauth2, ENV["GOOGLE_API_CLIENT_ID"], ENV["GOOGLE_API_CLIENT_SECRET"],
+    {
+      scope: "profile" # This requests access to "View your basic profile info"
+    }
 end
