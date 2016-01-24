e18416e19609e90e2c53f66bc4a1ac5d238f7d59
Alex Taylor <alexmctaylorpants@gmail.com>
2016-01-24 15:12:36 -0800

    Removes files and routes for alpha microsite

--- config/routes.rb
+++ config/routes.rb
@@ -37,24 +37,6 @@ Rails.application.routes.draw do
     end
   end
 
-  # For the 'survey' microsite.
-  namespace :alpha do
-    get "/beer_reviewer" => "reviews#start"
-    post "/beer_reviewer" => "reviews#save_user"
-    get "/end" => "reviews#end"
-
-    # admin
-    # get  "/admin/breweries/new" => "breweries#new"
-    # post "/admin/breweries" => "breweries#create"
-    # get  "/admin/breweries/:id" => "breweries#admin_show", as: :admin_brewery_show
-
-    resources :reviews, only: [:new, :create]
-    resources :breweries, only: [:index, :show] do
-      resources :beers
-    end
-    resources :beers, only: [:index]
-  end
-
   # Catch /ui requests and redirect it to React.
   # Wildcard matching requires a parameter, even though we're not going to use it
   # See http://guides.rubyonrails.org/routing.html#route-globbing-and-wildcard-segments
